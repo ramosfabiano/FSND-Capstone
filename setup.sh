7ce6bb0fe36830892db5858a2257f7f54d76ce38
Fabiano Ramos <ramos_fabiano@yahoo.com.br>
2023-10-06 16:30:54 -0300

    Adding extra info for submission purposes

--- setup.sh
+++ setup.sh
@@ -8,3 +8,7 @@ export ENABLE_AUTH=1
 export AUTH0_DOMAIN="dev-rvbtc77b8udvaibw.us.auth0.com" 
 export ALGORITHMS="RS256"
 export API_AUDIENCE="fsnd-capstone" 
+
+# for submission purposes
+export AUTH0_CLIENT_ID="GnZ9oyet4BRVLgbXc17klkbGVOsKRX81"
+export AUTH0_CLIENT_SECRET="l2PLVxrfo6YLodEmu3XCIkuyLLxs5e4HQsxYl0qDYHLzUeLd5IL3XMSFouf2pAHl"
